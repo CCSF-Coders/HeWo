235544772626b016e9854a547f62a9fafc90bb25
Kevin Morris <kevr@sonic.net>
2014-01-29 23:53:29 -0800

    Added c and perl; Deleted compiled files in repository

---
+++ HeWo.c
@@ -0,0 +1,18 @@
+/*
+ Project: Hello World
+ Author: CCSF Coders
+ Description: Prints "Hello World" in c
+
+ To compile: gcc HeWo.c -o HeWo
+ To run: ./HeWo
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Hello World! - English\n");
+    printf("Ciao Mondo! - Italian\n");
+    return 0;
+}
+
---
+++ HeWo.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+#
+# Project: Hello World
+# Author: CCSF Coders
+# Description: Prints "Hello World" in perl
+#
+# To run: perl HeWo.pl
+#
+
+print "Hello World! - English\n";
+print "Ciao Mondo! - Italian\n"; ## contributed by Jason
+
