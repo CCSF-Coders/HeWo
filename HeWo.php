5553fb09a6a8e513f20d249e79214bdf2d3f333e
Kevin Morris <kevr@sonic.net>
2014-01-31 02:18:38 -0800

    Added php, sh/bash, haskell, and zsh sources; setup python3 env and executable perms on pl,py,sh,zsh

---
+++ HeWo.php
@@ -0,0 +1,19 @@
+<?php
+
+    /*
+     Note: php supports C, C++ and UNIX-shell style (Perl style) comments
+     // This is a one-line C/C++ style comment
+     # This is a UNIX-shell style comment
+
+     Project: Hello World
+     Author: CCSF Coders
+     Description: Prints "Hello World" in php
+
+     To run: php HeWo.php
+     Or: host HeWo.php in a php-enabled http(s) server.
+    */
+
+    echo("Hello World! - English\n");
+    echo("Ciao Mondo! - Italian");
+?>
+
